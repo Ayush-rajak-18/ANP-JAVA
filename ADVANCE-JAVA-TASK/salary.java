43f76f98d5045ad3647e596598a6c23d52d8dc46
Ayush Rajak <www.amanr2004@gmail.com>
2024-10-09 20:50:36 +0530

    SalaryPercentage

---
+++ ADVANCE-JAVA-TASK/salary.java
@@ -0,0 +1,11 @@
+ class hello {
+
+    public static void main(String[] args) 
+{
+        double salary = 50000;
+        double percentage = 10.0 / 100.0;  
+        double result = salary * percentage;
+        
+        System.out.println(" (" + salary + ")  " + result);
+    }
+}
